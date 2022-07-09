f6a879d33a99d578924465fcbef8a18ad12f6da7
Umair Abbas <umairabbass786@gmail.com>
2022-07-09 22:58:29 +0500

    some modifications

--- includes/header.php
+++ includes/header.php
@@ -2,25 +2,21 @@
 <html lang="zxx" class="dark">
 
 <head>
-  <!-- metas -->
-  <meta charset="utf-8">
-  <meta name="author" content="pxdraft">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-  <meta name="keywords" content="Crikon – Multipurpose Template">
-  <meta name="description" content="Crikon – Multipurpose Template"><!-- title -->
-  <title>ShopApp - eCommerce Bootstrap 5 Template</title><!-- Favicon -->
-  <link rel="shortcut icon" href="assets/img/favicon.ico"><!-- CSS Template -->
-  <link href="assets/css/theme.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
-  
+    <!-- metas -->
+    <meta charset="utf-8">
+    <meta name="author" content="pxdraft">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Home | Fero Sports</title>
+    <link rel="shortcut icon" href="assets/img/favicon.ico">
+    <link href="assets/css/theme.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
+
 </head>
 
 <body>
+    <!-- Preload -->
+    <div id="loading" class="loading-preloader">
+        <div class="spinner-border text-primary" role="status"><span class="visually-hidden">Loading...</span></div>
+    </div><!-- End Preload -->
 
-<!-- Preload -->
-<div id="loading" class="loading-preloader">
-    <div class="spinner-border text-primary" role="status"><span class="visually-hidden">Loading...</span></div>
-  </div><!-- End Preload -->
-
-  <div class="wrapper">
+    <div class="wrapper">
